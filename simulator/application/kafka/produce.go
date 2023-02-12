74cf695478be0de9947ca895a2fbb61fc39573f5
wogel <rafael_wow19696@hotmail.com>
2023-02-12 19:17:07 -0300

    first commit

---
+++ simulator/application/kafka/produce.go
@@ -0,0 +1,7 @@
+package kafka
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route.NewRoute()
+	json.Unmarshal()
+}
---
+++ simulator/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
+	"github.com/rafazx/imersaofsfc2-simulator/infra/kafka"
+	"log"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+	}
+}
